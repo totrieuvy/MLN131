d6c325c1a5ac81a19318aad11dae1caf2f4d6e83
totrieuvy <vytotrieu12a121@gmail.com>
2025-10-29 23:03:41 +0700

    u

--- src/component/factors/Factor.jsx
+++ src/component/factors/Factor.jsx
@@ -13,12 +13,12 @@ const Factor = () => {
     {
       title: "Lãnh thổ chung",
       text: 'Đây không chỉ là không gian địa lý mà còn là "không gian sinh tồn" của dân tộc, bao gồm vùng đất, vùng trời, vùng biển thuộc chủ quyền quốc gia. Lãnh thổ là cơ sở vật chất cho các hoạt động kinh tế, văn hóa, xã hội và là yếu tố thiêng liêng gắn với khái niệm "Tổ quốc". Việc xác lập và bảo vệ chủ quyền lãnh thổ là nghĩa vụ và trách nhiệm cao nhất của mỗi thành viên trong dân tộc.',
-      img: "https://media.istockphoto.com/id/1455958730/vi/vec-to/gi%C3%A1m-s%C3%A1t-v%C3%A0-ph%C3%A2n-t%C3%ADch-d%E1%BB%AF-li%E1%BB%87u-tr%C3%AAn-m%C3%A1y-t%C3%ADnh-%C4%91%E1%BB%83-b%C3%A0n-nh%E1%BB%AFng-ng%C6%B0%E1%BB%9Di-nh%E1%BB%8F-b%C3%A9-v%E1%BB%9Bi-%C4%91%E1%BB%93-th%E1%BB%8B-nghi%C3%AAn-c%E1%BB%A9u.jpg?s=612x612&w=0&k=20&c=tcmfzeeTFDOxCyDY9LzR6HckAM21uBcb9EYPZBCF8Pw=",
+      img: "https://maisonoffice.vn/wp-content/uploads/2023/11/1-ban-do-the-gioi-va-cac-chau-luc-moi-nhat-hien-nay.jpg",
     },
     {
       title: "Ngôn ngữ chung",
       text: "Ngôn ngữ là công cụ giao tiếp thống nhất, giúp liên kết các thành viên trong cộng đồng trên mọi lĩnh vực. Nó không chỉ truyền tải thông tin mà còn là phương tiện lưu giữ, truyền bá văn hóa, lịch sử, và tạo nên sự đồng nhất về tư tưởng, tình cảm. Trong một quốc gia đa tộc người, thường sẽ có một ngôn ngữ chung để giao tiếp và quản lý.",
-      img: "https://cdn.nhandan.vn/images/90226d43541fe3e6433eed17cac8f63a2dc3784b3fb5ccc593be5348d47b44da376c3f16f3bc8cf0299f2ec17cc2b51c/sgggg-4734.jpg",
+      img: "https://png.pngtree.com/png-clipart/20200224/original/pngtree-globe-launguage-icon-simple-style-png-image_5248653.jpg",
     },
     {
       title: "Đời sống kinh tế chung",
@@ -28,7 +28,7 @@ const Factor = () => {
     {
       title: "Bản sắc văn hóa và ý thức tự giác dân tộc",
       text: "Yếu tố này bao gồm toàn bộ các giá trị văn hóa vật chất và tinh thần như phong tục, tập quán, tín ngưỡng, nghệ thuật, cùng với tâm lý, tính cách đặc thù được hình thành qua hàng ngàn năm lịch sử. Đi cùng với đó là ý thức tự giác, tức là sự tự nhận thức về sự thống nhất của cộng đồng mình, về nguồn gốc và bản sắc riêng, giúp phân biệt dân tộc này với dân tộc khác và tạo nên sức mạnh tinh thần to lớn.",
-      img: "https://maisonoffice.vn/wp-content/uploads/2023/11/1-ban-do-the-gioi-va-cac-chau-luc-moi-nhat-hien-nay.jpg",
+      img: "https://image.luatvietnam.vn/uploaded/twebp/images/original/2023/10/09/ban-sac-van-hoa-dan-toc-la-gi_0910185513.jpg",
     },
   ];
 
